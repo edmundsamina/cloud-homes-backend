938d6e172dde9ba1d1e904dccc678264aed2f135
edmundsamina <edmundsamina@gmail.com>
2022-12-09 11:07:52 +0000

    port changed

--- app.js
+++ app.js
@@ -1,12 +1,12 @@
 import express from "express";
 import propertiesRouter from "./routes/routes.js";
 const app = express();
+const PORT = process.env.PORT;
+
 
 app.use(express.json());
 app.use("/api/properties", propertiesRouter);
 
-
-const PORT = 3000;
 
 app.listen(PORT, function () {
   console.log(`Server is running on port ${PORT}`);
