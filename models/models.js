d503dae9b266972d107a84c63b4287a2198af6ea
ionarose <ionaroseuk@gmail.com>
2022-12-30 13:32:57 +0000

    filtering fixed

--- models/models.js
+++ models/models.js
@@ -14,27 +14,79 @@ export async function getAllProperties(){
      
 //search by  metaverse AND rent or buy
 export async function getPropertiesByMetaverse(metaverse, rent, type, bedrooms, bathrooms){
-    if(!bedrooms && !bathrooms){
-        const result = await query("SELECT * FROM properties WHERE metaverse LIKE $1 AND rent = $2 AND property_type LIKE $3", ['%' + metaverse.toUpperCase() + '%', rent, '%' +type + '%'])  
-        return result.rows
-    }   
-    if(!type){
-        const result = await query("SELECT * FROM properties WHERE metaverse LIKE $1 AND rent = $2 AND bedrooms = $3 AND bathrooms = $4", ['%' + metaverse.toUpperCase() + '%', rent,  bedrooms,  bathrooms])
-        return result.rows
+
+let params = []
+    let sqlquery = 'SELECT * FROM properties';
+     if(metaverse) {
+        sqlquery += ` WHERE metaverse = '${metaverse.toUpperCase()}'`;
+        params.push('%' + metaverse.toUpperCase() + '%');
+     }
+  
+     if (rent) {
+        if (params.length > 0) {
+            sqlquery += ' AND';
+          } else {
+            sqlquery += ' WHERE';
+          }
+          sqlquery += ` rent = ${rent}`;
+        params.push(rent);
+      }
+      if (type) {
+        if (params.length > 0) {
+            sqlquery += ' AND';
+          } else {
+            sqlquery += ' WHERE';
+          }
+          sqlquery += ` property_type = '${type}'`;
+        params.push(type);
+      }
+    if (bedrooms > 0) {
+        if (params.length > 0) {
+            sqlquery += ' AND';
+          } else { 
+            sqlquery += ' WHERE';
+          }
+          sqlquery += ` bedrooms = ${bedrooms}`;
+      params.push(bedrooms);
     }
-    else if(!bedrooms){
-        const result = await query("SELECT * FROM properties WHERE metaverse LIKE $1 AND rent = $2 AND property_type LIKE $3  AND bathrooms = $4", ['%' + metaverse.toUpperCase() + '%', rent, '%' +type + '%', bathrooms])
-        return result.rows
+  
+    if (bathrooms > 0) {
+      if (params.length > 0) {
+        sqlquery += ' AND';
+      } else {
+        sqlquery += ' WHERE';
+      }
+      sqlquery += ` bathrooms = ${bathrooms}`;
+      params.push(bathrooms);
     }
-    else if(!bathrooms){
-        const result = await query("SELECT * FROM properties WHERE metaverse LIKE $1 AND rent = $2 AND property_type LIKE $3 AND bedrooms = $4 ", ['%' + metaverse.toUpperCase() + '%', rent, '%' +type + '%', bedrooms])
-        return result.rows
-    } else if(metaverse && rent && type && bedrooms && bathrooms){
+    console.log(sqlquery)
+    console.log(params)
+  
+    const result = await query(sqlquery)
+    return result.rows
+
+
+    // if(!bedrooms && !bathrooms){
+    //     const result = await query("SELECT * FROM properties WHERE metaverse LIKE $1 AND rent = $2 AND property_type LIKE $3", ['%' + metaverse.toUpperCase() + '%', rent, '%' +type + '%'])  
+    //     return result.rows
+    // }   
+    // if(!type){
+    //     const result = await query("SELECT * FROM properties WHERE metaverse LIKE $1 AND rent = $2 AND bedrooms = $3 AND bathrooms = $4", ['%' + metaverse.toUpperCase() + '%', rent,  bedrooms,  bathrooms])
+    //     return result.rows
+    // }
+    // else if(!bedrooms){
+    //     const result = await query("SELECT * FROM properties WHERE metaverse LIKE $1 AND rent = $2 AND property_type LIKE $3  AND bathrooms = $4", ['%' + metaverse.toUpperCase() + '%', rent, '%' +type + '%', bathrooms])
+    //     return result.rows
+    // }
+    // else if(!bathrooms){
+    //     const result = await query("SELECT * FROM properties WHERE metaverse LIKE $1 AND rent = $2 AND property_type LIKE $3 AND bedrooms = $4 ", ['%' + metaverse.toUpperCase() + '%', rent, '%' +type + '%', bedrooms])
+    //     return result.rows
+    // } else if(metaverse && rent && type && bedrooms && bathrooms){
           
-        const result = await query("SELECT * FROM properties WHERE metaverse LIKE $1 AND rent = $2 AND property_type LIKE $3 AND bedrooms = $4 AND bathrooms = $5", ['%' + metaverse.toUpperCase() + '%', rent, '%' +type + '%',  bedrooms,  bathrooms])
+    //     const result = await query("SELECT * FROM properties WHERE metaverse LIKE $1 AND rent = $2 AND property_type LIKE $3 AND bedrooms = $4 AND bathrooms = $5", ['%' + metaverse.toUpperCase() + '%', rent, '%' +type + '%',  bedrooms,  bathrooms])
     
-        return result.rows
-    }   
+    //     return result.rows
+    // }   
       
 }      
 
