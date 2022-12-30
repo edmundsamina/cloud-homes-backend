1cabd265f289f9e9d395ba92626f809dc465f906
edmundsamina <edmundsamina@gmail.com>
2022-12-30 09:50:08 +0000

    alternative routes added

--- models/models.js
+++ models/models.js
@@ -1,21 +1,25 @@
 import query from "../db/db.js"
 
-//return 
+//return           
 export async function getAllProperties(){
     const allProperties = await query("SELECT * FROM properties")
     return allProperties.rows
 }
-
-// //search by  metaverse
+      
+// //search by  metaverse   
 // export async function getPropertiesByMetaverse(metaverse){
 //     const result = await query("SELECT * FROM properties WHERE metaverse LIKE $1", ['%' + metaverse.toUpperCase() + '%'])
 //     return result.rows
-// }  
-
+// }    
+     
 //search by  metaverse AND rent or buy
 export async function getPropertiesByMetaverse(metaverse, rent, type, bedrooms, bathrooms){
     if(!bedrooms && !bathrooms){
         const result = await query("SELECT * FROM properties WHERE metaverse LIKE $1 AND rent = $2 AND property_type LIKE $3", ['%' + metaverse.toUpperCase() + '%', rent, '%' +type + '%'])  
+        return result.rows
+    }   
+    if(!type){
+        const result = await query("SELECT * FROM properties WHERE metaverse LIKE $1 AND rent = $2 AND bedrooms = $3 AND bathrooms = $4", ['%' + metaverse.toUpperCase() + '%', rent,  bedrooms,  bathrooms])
         return result.rows
     }
     else if(!bedrooms){
@@ -26,13 +30,13 @@ export async function getPropertiesByMetaverse(metaverse, rent, type, bedrooms, 
         const result = await query("SELECT * FROM properties WHERE metaverse LIKE $1 AND rent = $2 AND property_type LIKE $3 AND bedrooms = $4 ", ['%' + metaverse.toUpperCase() + '%', rent, '%' +type + '%', bedrooms])
         return result.rows
     } else if(metaverse && rent && type && bedrooms && bathrooms){
-       
+          
         const result = await query("SELECT * FROM properties WHERE metaverse LIKE $1 AND rent = $2 AND property_type LIKE $3 AND bedrooms = $4 AND bathrooms = $5", ['%' + metaverse.toUpperCase() + '%', rent, '%' +type + '%',  bedrooms,  bathrooms])
     
         return result.rows
-    }
-   
-} 
+    }   
+      
+}      
 
     
 //get question by name
